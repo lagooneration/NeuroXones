324f2b1fb212eff0fa1caf244b82938f69d7d0a9
lagooneration <puneetkumarlagoo@gmail.com>
2025-06-02 23:59:41 +0530

    New Scene

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -7,8 +7,8 @@ import { Room } from "./Room";
 import HeroLights from "./HeroLights";
 import Particles from "./Particles";
 import { Suspense } from "react";
-import { Headphones } from "../../Headphones";
-import { Neuroxones } from "../../Neuroxones";
+import { Neuroxone } from "../../models/Neuroxone";
+import { Brain } from "../../models/Brain";
 import BackgroundShader from "../../BackgroundShader";
 
 
@@ -17,7 +17,7 @@ const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
-    <Canvas camera={{ position: [0, 8, 15], fov: 45 }}>
+    <Canvas camera={{ position: [2, 8, 5], fov: 45 }}>
       {/* deep blue ambient */}
       <ambientLight intensity={0.2} color="#1a1a40" />
       {/* Configure OrbitControls to disable panning and control zoom based on device type */}
@@ -34,19 +34,20 @@ const HeroExperience = () => {
 
         <HeroLights />
         {/* <Particles count={100} /> */}
-        <BackgroundShader />
+        {/* <BackgroundShader /> */}
         <group
           scale={isMobile ? 26 : 26}
           position={[0, -2.5, 0]} // Changed position to center
           rotation={[-Math.PI / 6, Math.PI / 2, 0]}
         >
           {/* <Room /> */}
-          <Neuroxones />
+          <Brain />
+          <Neuroxone />
         </group>
       </Suspense>
-      <EffectComposer>
+      {/* <EffectComposer>
         <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-      </EffectComposer>
+      </EffectComposer> */}
     </Canvas>
   );
 };
